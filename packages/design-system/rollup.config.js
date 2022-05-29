1d1ac14646818fcdc08ef7531211db30a9d1c386
Annshuk Sharma <annshuks@gmail.com>
2022-05-29 17:03:07 +0530

    :up: updated configuration

--- packages/design-system/rollup.config.js
+++ packages/design-system/rollup.config.js
@@ -1,5 +1,5 @@
 import babel from "@rollup/plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
@@ -11,13 +11,14 @@ export default {
     { file: pkg.module, format: "esm" },
   ],
   plugins: [
+    nodeResolve({
+      extensions: [".js", ".ts", ".tsx"],
+    }),
+    commonjs(),
     babel({
       babelHelpers: "bundled",
       exclude: "node_modules/**",
-      presets: ["@babel/preset-env", "@babel/preset-react"],
     }),
-    resolve(),
-    commonjs(),
     terser(),
   ],
   external: Object.keys(pkg.peerDependencies),
