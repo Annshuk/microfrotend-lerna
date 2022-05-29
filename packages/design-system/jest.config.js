803f476b8ac1be382a91ae8400c320c9acbe258f
Annshuk Sharma <annshuks@gmail.com>
2022-05-29 16:40:26 +0530

    :moon: updated rollup and jest config

---
+++ packages/design-system/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  roots: ["./src"],
+  setupFilesAfterEnv: ["./jest.setup.js"],
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  testPathIgnorePatterns: ["node_modules/"],
+  testMatch: ["**/*.test.(js|ts|tsx)"],
+  moduleNameMapper: {
+    // Mocks out all these file formats when tests are run
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "identity-obj-proxy",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+};
---
+++ packages/design-system/src/components/Button/tests/Button.test.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Button from "../Button";
+
+describe("Test Component", () => {
+  const renderComponent = () => render(<Button {...props} />);
+
+  it("should have primary className with default props", () => {
+    const { getByTestId } = renderComponent();
+
+    const testComponent = getByTestId("test-component");
+
+    expect(testComponent).toHaveClass("test-component-primary");
+  });
+
+  it("should have secondary className with theme set as secondary", () => {
+    props.theme = "secondary";
+    const { getByTestId } = renderComponent();
+
+    const testComponent = getByTestId("test-component");
+
+    expect(testComponent).toHaveClass("test-component-secondary");
+  });
+});
