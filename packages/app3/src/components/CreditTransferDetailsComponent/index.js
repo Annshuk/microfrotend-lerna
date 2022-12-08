9ed4328b23511788b14bc53b9c09797b6e2ae126
Anshuk Sharma <annshuks@gmail.com>
2022-12-08 09:50:26 +0530

    :+1: updated

--- packages/app3/src/components/CreditTransferDetailsComponent/index.js
+++ packages/app3/src/components/CreditTransferDetailsComponent/index.js
@@ -1,63 +1,57 @@
-import { forwardRef } from 'react';
-import { Input, Label, FormGroup, FormFeedback } from 'reactstrap'
 import { InputLabelField } from "../InputLabelField";
 import { Heading } from '../Typography/Heading';
 import { Fieldset } from '../Fieldset';
 import { Checkbox } from '../Checkbox';
-import { Form, Col, Row, Button, Alert } from 'reactstrap';
-import { useForm, FormProvider } from 'react-hook-form';
-import { useQuery, useMutation } from 'react-query';
-import { getQuery, postQuery } from '../../services';
+import { Col, Row, } from 'reactstrap';
+import { useFormContext } from 'react-hook-form';
+
 
 /**
- * InputLabelField
- * input label field
+ * CreditTransferDetailsComponent
  * 
  */
-export const CreditTransferDetailsComponent = forwardRef(({ name, label, id, errors,BtchBookg,creatorName,financialInit,creditorAccount,paymentID,account,transcationMessage,categoryPurpose,serviceLevel, ...rest }, ref) => {
-    const { data: payments } = useQuery('payments', getQuery);
-	const formProps = useForm({ defaultValues: payments });	
-    const { control, handleSubmit, register } = formProps;   
+export const CreditTransferDetailsComponent = () => {
+    const { register } = useFormContext();
 
-   return (<Fieldset >
-                <Heading title="Credit Transfer Details" variant='h2' />
-                <Row>
-                    <Checkbox label="Batch Booking"  { ...register('BtchBookg') } id="btchBookg" />
-                </Row>
-                <Row>
-                    <Col md={ 6 }>
-                        <InputLabelField label="Creditor Name" { ...register('creatorName') } id="creditor-name" />
-                    </Col>
-                    <Col md={ 6 }>
-                        <InputLabelField label="Financial Institution Id" { ...register('financialInit') } id="financial-institution" placeholder="BICFI" />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={ 6 }>
-                        <InputLabelField label="Creditor Account" { ...register('creditorAccount') } placeholder="IBAN" id="creditro-ac" />
+    return (<Fieldset >
+        <Heading title="Credit Transfer Details" variant='h2' />
+        <Row>
+            <Checkbox label="Batch Booking"  { ...register('BtchBookg') } id="btchBookg" />
+        </Row>
+        <Row>
+            <Col md={ 6 }>
+                <InputLabelField label="Creditor Name" { ...register('creatorName') } id="creditor-name" />
+            </Col>
+            <Col md={ 6 }>
+                <InputLabelField label="Financial Institution Id" { ...register('financialInit') } id="financial-institution" placeholder="BICFI" />
+            </Col>
+        </Row>
+        <Row>
+            <Col md={ 6 }>
+                <InputLabelField label="Creditor Account" { ...register('creditorAccount') } placeholder="IBAN" id="creditro-ac" />
 
-                    </Col>
-                    <Col md={ 6 }>
-                        <InputLabelField label="Payment ID" { ...register('paymentID') } placeholder="End To End transaction Id" id="paymentId" />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={ 6 }>
-                        <InputLabelField label="Account" { ...register('account') } id="account" />
-                    </Col>
-                    <Col md={ 6 }>
-                        <InputLabelField label="Remittance Information" { ...register('transcationMessage') } id="transcationMessage" readOnly placeholder="Unstructured Remittance Information" />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={ 6 }>
-                        <InputLabelField label="Category Purpose" { ...register('categoryPurpose') } id="categoryPurpose" placeholder="Payment Purpose" />
-                    </Col>
-                    <Col md={ 6 }>
-                        <InputLabelField label="Service Level" { ...register('serviceLevel') } id="serviceLevel" placeholder="End To End transaction Id" />
-                    </Col>
-                </Row>
-            </Fieldset>
+            </Col>
+            <Col md={ 6 }>
+                <InputLabelField label="Payment ID" { ...register('paymentID') } placeholder="End To End transaction Id" id="paymentId" />
+            </Col>
+        </Row>
+        <Row>
+            <Col md={ 6 }>
+                <InputLabelField label="Account" { ...register('account') } id="account" />
+            </Col>
+            <Col md={ 6 }>
+                <InputLabelField label="Remittance Information" { ...register('transcationMessage') } id="transcationMessage" readOnly placeholder="Unstructured Remittance Information" />
+            </Col>
+        </Row>
+        <Row>
+            <Col md={ 6 }>
+                <InputLabelField label="Category Purpose" { ...register('categoryPurpose') } id="categoryPurpose" placeholder="Payment Purpose" />
+            </Col>
+            <Col md={ 6 }>
+                <InputLabelField label="Service Level" { ...register('serviceLevel') } id="serviceLevel" placeholder="End To End transaction Id" />
+            </Col>
+        </Row>
+    </Fieldset>
     )
-})
+}
 
--- packages/app3/src/pages/EditPayment.js
+++ packages/app3/src/pages/EditPayment.js
@@ -1,12 +1,10 @@
 import { Form, Col, Row, Button, Alert } from 'reactstrap';
-import { useForm, useFieldArray, FormProvider } from 'react-hook-form';
-import { useQuery, useMutation  } from 'react-query';
+import { useForm, FormProvider } from 'react-hook-form';
+import { useQuery, useMutation } from 'react-query';
 import { Box, Flex } from 'rebass';
 
 import {
-  atom,
-  useRecoilState,
-  useRecoilValue,
+    useRecoilState,
 } from 'recoil';
 
 import { InputLabelField } from "../components/InputLabelField";
@@ -15,52 +13,45 @@ import { Fieldset } from '../components/Fieldset';
 import { Checkbox } from '../components/Checkbox';
 import { CreditTransferDetailsComponent } from '../components/CreditTransferDetailsComponent';
 
-import { getQuery, postQuery, convertDateToIso, convertIsoDateToLocal } from '../services';
+import { getQuery, postQuery, convertDateToIso } from '../services';
 import { appState } from '../recoils/atoms';
 import { useNavigate } from "react-router-dom";
 import { InputLabelReadonlyField } from "../components/InputLabelReadonlyField";
 
 
 export const EditPayment = () => {
-	
-	const navigate = useNavigate();
-	const [appDefaultData, setAppDefaultData] = useRecoilState(appState);
-	console.log("setAppDefaultData", appDefaultData);
-    const { data: payments } = useQuery('payments', getQuery);
-	
-    const formProps = useForm({ defaultValues: appDefaultData });	
-	const { mutate, isError, data } = useMutation(postQuery);	
-    const { control, handleSubmit, register } = formProps;
-	
-    const onSubmit = (formValue) => {
 
-        console.warn("postData", formValue);
-		formValue.CreDtTm = convertDateToIso(formValue.CreDtTm);
-		
-		//save form value in state
-		setAppDefaultData(formValue);
-		
+    const navigate = useNavigate();
+    const [appDefauls, setFormState] = useRecoilState(appState);
+    const { data: payments } = useQuery('payments', getQuery);
+
+    const formProps = useForm({ defaultValues: appDefauls });
+
+    const { handleSubmit, register } = formProps;
+
+    const onSubmit = (formValue) => {
+        formValue.CreDtTm = convertDateToIso(formValue.CreDtTm);
+        //save form value in state
+        setFormState(formValue);
+
         navigate("/view-and-confirm");
     }
 
     return <Box as={ Form } py="20px">
-        { isError && <Alert color="danger">
-            This is a primary alert — check it out!
-        </Alert> }
         <FormProvider { ...formProps }>
             <Fieldset>
                 <Heading title="Customer Credit Transfer Details" variant='h1' />
                 <Row>
                     <Col md={ 6 }>
-                        <InputLabelField label="Credit Transaction Date"  { ...register('CreDtTm', { required: true, value: appDefaultData.CreDtTm }) } type="datetime-local" id="CreDtTm" placeholder="Date" />
+                        <InputLabelField label="Credit Transaction Date"  { ...register('CreDtTm', { required: true }) } type="datetime-local" id="CreDtTm" placeholder="Date" />
                     </Col>
                     <Col md={ 6 }>
-                        <InputLabelField readOnly label="Transaction Control Sum" id="CtrlSum"  { ...register('CtrlSum', { value: appDefaultData.CtrlSum }) } type="text" placeholder="Control Sum value" />
+                        <InputLabelField readOnly label="Transaction Control Sum" id="CtrlSum"  { ...register('CtrlSum') } type="text" placeholder="Control Sum value" />
                     </Col>
                 </Row>
                 <Row>
                     <Col md={ 6 }>
-                        <InputLabelField label="Payment Initiator Country" type="select" id="Ctry"  { ...register('Ctry', { value: appDefaultData.Ctry }) }>
+                        <InputLabelField label="Payment Initiator Country" type="select" id="Ctry"  { ...register('Ctry') }>
                             <option>Choose...</option>
                             <option value="India">India</option>
                             <option value="US">US</option>
@@ -68,97 +59,97 @@ export const EditPayment = () => {
                         </InputLabelField>
                     </Col>
                     <Col md={ 6 }>
-                        <InputLabelField label="Payment Initiator Name" id="Nm"  { ...register('Nm', { value: appDefaultData.Nm }) } />
+                        <InputLabelField label="Payment Initiator Name" id="Nm"  { ...register('Nm') } />
                     </Col>
                 </Row>
                 <Row>
                     <Col md={ 6 }>
-                        <InputLabelField label="Number of Transactions" type="number" id="NbOfTxs" { ...register('NbOfTxs', { value: appDefaultData.NbOfTxs }) } placeholder="" />
+                        <InputLabelField label="Number of Transactions" type="number" id="NbOfTxs" { ...register('NbOfTxs') } placeholder="" />
                     </Col>
                     <Col md={ 6 }>
-                        <InputLabelReadonlyField label="Transaction Message ID" id="MsgId" { ...register('MsgId', { value: appDefaultData.MsgId }) } placeholder="" errors="" />
+                        <InputLabelReadonlyField label="Transaction Message ID" id="MsgId" { ...register('MsgId') } placeholder="" errors="" />
                     </Col>
                 </Row>
             </Fieldset>
 
-            <Heading title="Payment Information" variant='h1' />            
-			
-			<Fieldset >
+            <Heading title="Payment Information" variant='h1' />
+
+            <Fieldset >
                 <Heading title="Credit Transfer Details" variant='h2' />
                 <Row>
-                    <Checkbox label="Batch Booking" checked={appDefaultData.BtchBookg}  { ...register('BtchBookg') } id="BtchBookg" name="BtchBookg" />
+                    <Checkbox label="Batch Booking" checked={ appDefauls.BtchBookg }  { ...register('BtchBookg') } id="BtchBookg" name="BtchBookg" />
                 </Row>
                 <Row>
                     <Col md={ 6 }>
-                        <InputLabelField label="Creditor Name" { ...register('creatorName', { value: appDefaultData.creatorName }) } id="creatorName" />
+                        <InputLabelField label="Creditor Name" { ...register('creatorName') } id="creatorName" />
                     </Col>
                     <Col md={ 6 }>
-                        <InputLabelField label="Financial Institution Id" { ...register('financialInit', { value: appDefaultData.financialInit }) } id="financial-institution" placeholder="BICFI" />
+                        <InputLabelField label="Financial Institution Id" { ...register('financialInit') } id="financial-institution" placeholder="BICFI" />
                     </Col>
                 </Row>
                 <Row>
                     <Col md={ 6 }>
-                        <InputLabelField label="Creditor Account" { ...register('creditorAccount', { value: appDefaultData.creditorAccount }) } placeholder="IBAN" id="creditro-ac" />
+                        <InputLabelField label="Creditor Account" { ...register('creditorAccount') } placeholder="IBAN" id="creditro-ac" />
 
                     </Col>
                     <Col md={ 6 }>
-                        <InputLabelField label="Payment ID" { ...register('paymentID', { value: appDefaultData.paymentID }) } placeholder="End To End transaction Id" id="paymentId" />
+                        <InputLabelField label="Payment ID" { ...register('paymentID') } placeholder="End To End transaction Id" id="paymentId" />
                     </Col>
                 </Row>
                 <Row>
                     <Col md={ 6 }>
-                        <InputLabelField label="Account" { ...register('account', { value: appDefaultData.account }) } id="account" />
+                        <InputLabelField label="Account" { ...register('account') } id="account" />
                     </Col>
                     <Col md={ 6 }>
-                        <InputLabelField label="Remittance Information" { ...register('transcationMessage', { value: appDefaultData.transcationMessage }) } id="transcationMessage" readOnly placeholder="Unstructured Remittance Information" />
+                        <InputLabelField label="Remittance Information" { ...register('transcationMessage') } id="transcationMessage" readOnly placeholder="Unstructured Remittance Information" />
                     </Col>
                 </Row>
                 <Row>
                     <Col md={ 6 }>
-                        <InputLabelField label="Category Purpose" { ...register('categoryPurpose', { value: appDefaultData.categoryPurpose }) } id="categoryPurpose" placeholder="Payment Purpose" />
+                        <InputLabelField label="Category Purpose" { ...register('categoryPurpose') } id="categoryPurpose" placeholder="Payment Purpose" />
                     </Col>
                     <Col md={ 6 }>
-                        <InputLabelField label="Service Level" { ...register('serviceLevel', { value: appDefaultData.serviceLevel }) } id="serviceLevel" placeholder="End To End transaction Id" />
+                        <InputLabelField label="Service Level" { ...register('serviceLevel') } id="serviceLevel" placeholder="End To End transaction Id" />
                     </Col>
                 </Row>
             </Fieldset>
-			
+
             <Fieldset>
 
                 <Heading title="Debitor  Details" as="h2" />
 
                 <Row>
                     <Col md={ 6 }>
-                        <InputLabelField label="Debitor Account" { ...register('debitorAccount', { value: appDefaultData.debitorAccount }) } id="debitorAccount" placeholder="IBAN" />
+                        <InputLabelField label="Debitor Account" { ...register('debitorAccount') } id="debitorAccount" placeholder="IBAN" />
                     </Col>
                     <Col md={ 6 }>
-                        <InputLabelField label="Required Execution Date" { ...register('requiredExedate', { value: appDefaultData.requiredExedate }) } type="date" id="requiredExedate" placeholder="Date" />
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col md={ 6 }>
-                        <InputLabelField label="Debitor Financial Institution Id" { ...register('debitorInstId', { value: appDefaultData.debitorInstId }) } id="debitorInstId" placeholder="SLEV" />
-                    </Col>
-                    <Col md={ 6 }>
-                        <InputLabelField label="Charge Info"  { ...register('chargeInfo', { value: appDefaultData.chargeInfo }) } id="chargeInfo" placeholder="IBAN" />
+                        <InputLabelField label="Required Execution Date" { ...register('requiredExedate') } type="date" id="requiredExedate" placeholder="Date" />
                     </Col>
                 </Row>
 
                 <Row>
                     <Col md={ 6 }>
-                        <InputLabelField label="Payment Information ID"  { ...register('paymentInformationID', { value: appDefaultData.paymentInformationID }) } id="paymentInformationID" placeholder="Payment Information ID" />
+                        <InputLabelField label="Debitor Financial Institution Id" { ...register('debitorInstId') } id="debitorInstId" placeholder="SLEV" />
+                    </Col>
+                    <Col md={ 6 }>
+                        <InputLabelField label="Charge Info"  { ...register('chargeInfo') } id="chargeInfo" placeholder="IBAN" />
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col md={ 6 }>
+                        <InputLabelField label="Payment Information ID"  { ...register('paymentInformationID') } id="paymentInformationID" placeholder="Payment Information ID" />
 
                     </Col>
                     <Col md={ 6 }>
-                        <InputLabelField label="Control Sum" { ...register('controlSum', { value: appDefaultData.controlSum }) } id="controlSum" placeholder="Control Sum" />
+                        <InputLabelField label="Control Sum" { ...register('controlSum') } id="controlSum" placeholder="Control Sum" />
 
                     </Col>
                 </Row>
 
                 <Row>
                     <Col md={ 6 }>
-                        <InputLabelField label="Debitor Country"  { ...register('debitorCountry', { value: appDefaultData.debitorCountry }) } type="select" id="debitorCountry" >
+                        <InputLabelField label="Debitor Country"  { ...register('debitorCountry') } type="select" id="debitorCountry" >
                             <option>Choose...</option>
                             <option value="India">India</option>
                             <option value="US">US</option>
@@ -167,17 +158,17 @@ export const EditPayment = () => {
 
                     </Col>
                     <Col md={ 6 }>
-                        <InputLabelField label="Debitor Name"  { ...register('debitorName', { value: appDefaultData.debitorName }) } id="debitorName" />
+                        <InputLabelField label="Debitor Name"  { ...register('debitorName') } id="debitorName" />
                     </Col>
                 </Row>
 
 
                 <Row>
                     <Col md={ 6 }>
-                        <InputLabelField label="Number of Transactions"  type="number" { ...register('paymentNoOfTranscation', { value: appDefaultData.paymentNoOfTranscation }) } id="paymentNoOfTranscation" />
+                        <InputLabelField label="Number of Transactions" type="number" { ...register('paymentNoOfTranscation') } id="paymentNoOfTranscation" />
                     </Col>
                     <Col md={ 6 }>
-                        <InputLabelField label="Payment Methods"  { ...register('paymentMethods', { value: appDefaultData.paymentMethods }) } id="paymentMethods" />
+                        <InputLabelField label="Payment Methods"  { ...register('paymentMethods') } id="paymentMethods" />
                     </Col>
                 </Row>
             </Fieldset>
@@ -186,7 +177,7 @@ export const EditPayment = () => {
                     color="primary"
                     onClick={ handleSubmit(onSubmit) }
                 >
-                    Confirm
+                    Next
                 </Button>
             </Flex>
 
