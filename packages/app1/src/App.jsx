70fab2d0a9b27c95a58fbd10a4a549dd5369020d
Anshuk Sharma <annshuks@gmail.com>
2025-03-01 20:59:44 +0530

    updated

--- packages/app1/src/App.jsx
+++ packages/app1/src/App.jsx
@@ -2,23 +2,20 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
+import {RootRouter} from './RootRouter'
 import {RemoteApp} from './RemoteApp'
 
 const  App = () => {
-
-  console.warn("DS")
-
-  
   return (
     <div className="App"> 
       <h1>Root Application</h1>
-      <BrowserRouter>
+      <RootRouter>
       <RemoteApp name='app3' url='http://localhost:3003/remoteEntry.js' module="App"/>
       <Routes>
         <Route path="/app2" element={<RemoteApp name='app2'  url="http://localhost:3002/remoteEntry.js"  module="App" />}/>
       </Routes>
-      <Outlet/>
-      </BrowserRouter>
-    </div>)}
+      </RootRouter>
+    </div>)
+  }
 
 export default App;
